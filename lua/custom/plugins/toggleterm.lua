d73c905ff071b74a0dee0950a4c737719d86f834
Alix Macdonald <amacdonald@bscglobal.com>
2024-11-21 21:28:33 +0000

    feat[term]: added toggle term

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    open_mapping = [[<leader>tt]],
+    direction = 'float',
+    float_opts = {
+      border = 'curved',
+      title_pos = 'center',
+    },
+  },
+}
