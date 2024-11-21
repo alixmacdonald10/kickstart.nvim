3dd0224213e74633d5e3ade36f5d51e0afe9de9d
Alix Macdonald <amacdonald@bscglobal.com>
2024-11-21 18:45:17 +0000

    fix[keybindings]: added buffer nav keybindings

--- init.lua
+++ init.lua
@@ -102,6 +102,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- key bindings for buffer nav
+vim.keymap.set('n', '<leader>bn', '<Cmd>bprevious<CR>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<leader>bp', '<Cmd>bnext<CR>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<leader>bc', '<Cmd>bdelete<CR>', { desc = 'Previous buffer' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
