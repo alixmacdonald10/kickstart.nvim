6629111e2c212acf703b338615e032c0eb525a60
Alix Macdonald <amacdonald@bscglobal.com>
2024-11-21 12:14:20 +0000

    fix: removed rust-analyzer from mason as this is managed by rustacean.nvim

--- init.lua
+++ init.lua
@@ -634,7 +634,6 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
-        rust = { 'rust_analyzer' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
