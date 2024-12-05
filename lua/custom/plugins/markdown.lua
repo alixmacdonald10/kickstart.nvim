f53362905abcbd0ca6ba800144a0b4b0b05444bb
Alix Macdonald <amacdonald@bscglobal.com>
2024-12-05 08:55:37 +0000

    feat[markdown]: added image rendering

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -13,4 +13,18 @@ return {
             vim.api.nvim_set_keymap('n', '<leader>tm', ':RenderMarkdown toggle<CR>', { noremap = true, silent = true })
         end,
     },
+    {
+        '3rd/image.nvim',
+        opts = {
+            backend = "kitty",
+            markdown = {
+                enabled = true,
+                clear_in_insert_mode = false,
+                download_remote_images = true,
+                only_render_image_at_cursor = true,
+                floating_windows = false,
+                filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+            },
+        },
+    },
 }
