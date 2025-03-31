2460e27e57e5db4dcdba53b4ee5edb796f567b5c
amac <amacdonald@bscglobal.com>
2025-03-31 07:06:54 +0000

    fix[rust]: improved rust keymaps with descriptions

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -14,12 +14,12 @@ return {
                 -- LSP configuration
                 server = {
                     on_attach = function(client, bufnr)
-                        vim.keymap.set('n', '<leader>lo', ':RustLsp openDocs<CR>', { noremap = true, silent = true })
-                        vim.keymap.set('n', '<leader>le', ':RustLsp explainError<CR>', { noremap = true, silent = true })
-                        vim.keymap.set('n', '<leader>lwr', ':RustLsp reloadWorkspace<CR>', { noremap = true, silent = true })
-                        vim.keymap.set('n', '<leader>lr', ':RustLsp runnables<CR>', { noremap = true, silent = true })
-                        vim.keymap.set('n', '<leader>ldb', ':RustLsp debug<CR>', { noremap = true, silent = true })
-                        vim.keymap.set('n', '<leader>ldd', ':lua require("dap").continue()<CR>', { noremap = true, silent = true })
+                        vim.keymap.set('n', '<leader>lo', ':RustLsp openDocs<CR>', { noremap = true, silent = true, desc = 'Open Docs' })
+                        vim.keymap.set('n', '<leader>le', ':RustLsp explainError<CR>', { noremap = true, silent = true, desc = 'Explain Error'})
+                        vim.keymap.set('n', '<leader>lwr', ':RustLsp reloadWorkspace<CR>', { noremap = true, silent = true, desc = 'Reload Workspace'})
+                        vim.keymap.set('n', '<leader>lr', ':RustLsp runnables<CR>', { noremap = true, silent = true, desc = 'Runnables'})
+                        vim.keymap.set('n', '<leader>ldb', ':RustLsp debug<CR>', { noremap = true, silent = true, desc = 'Debug'})
+                        vim.keymap.set('n', '<leader>ldd', ':lua require("dap").continue()<CR>', { noremap = true, silent = true, desc = 'Debugables' })
                     end,
                     default_settings = {
                         -- rust-analyzer language server configuration
@@ -58,10 +58,10 @@ return {
                     require("neotest-rust")
                 }
             })
-            vim.keymap.set('n', '<leader>lt', ':Neotest<CR>', { noremap = true, silent = true })
-            vim.keymap.set('n', '<leader>ltr', ':Neotest run<CR>', { noremap = true, silent = true })
-            vim.keymap.set('n', '<leader>ltp', ':Neotest output-panel<CR>', { noremap = true, silent = true })
-            vim.keymap.set('n', '<leader>lts', ':Neotest summary<CR>', { noremap = true, silent = true })
+            vim.keymap.set('n', '<leader>lt', ':Neotest<CR>', { noremap = true, silent = true, desc = 'Neotest' })
+            vim.keymap.set('n', '<leader>ltr', ':Neotest run<CR>', { noremap = true, silent = true, desc = 'Run Tests'})
+            vim.keymap.set('n', '<leader>ltp', ':Neotest output-panel<CR>', { noremap = true, silent = true, desc = 'Test Panel' })
+            vim.keymap.set('n', '<leader>lts', ':Neotest summary<CR>', { noremap = true, silent = true, desc = 'Test Summary'})
         end
     },
 }
