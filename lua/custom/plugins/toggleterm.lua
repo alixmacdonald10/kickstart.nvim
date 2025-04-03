caee5ddf4cf52cbff9b11faacce3210605bf0e37
amac <amacdonald@bscglobal.com>
2025-04-03 10:35:25 +0100

    fix[toggleterm]: disabled spell check in toggle terminal windows

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,10 +1,22 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-  opts = {
-    open_mapping = [[<leader>tt]],
-    direction = 'vertical',
-    size = 75,
-    persist_size = true
-  },
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<leader>tt]],
+      direction = 'vertical',
+      size = 75,
+      persist_size = true
+    })
+    
+    -- Create an autocommand to disable spell check in ToggleTerm buffers
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "*",
+      callback = function()
+        if vim.bo.filetype == "toggleterm" then
+          vim.opt_local.spell = false
+        end
+      end,
+    })
+  end,
 }
