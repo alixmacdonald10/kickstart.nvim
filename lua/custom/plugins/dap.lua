596e438528d14ba172d857e4d4b4f9b9bf6d9810
amackerel <amackerel@proton.me>
2026-08-12 13:06:07 +0100

    feat[dap]: added dap

---
+++ lua/custom/plugins/dap.lua
@@ -0,0 +1,66 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
+
+      require("dapui").setup()
+
+      require("nvim-dap-virtual-text").setup {
+        -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+        display_callback = function(variable)
+          local name = string.lower(variable.name)
+          local value = string.lower(variable.value)
+          if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+            return "*****"
+          end
+
+          if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+          end
+
+          return " " .. variable.value
+        end,
+      }
+
+      dap.configurations.rust = { 
+
+      }
+
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<leader>dgb", dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set("n", "<leader>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", continue, dap.continue)
+      vim.keymap.set("n", step_into, dap.step_into)
+      vim.keymap.set("n", step_over, dap.step_over)
+      vim.keymap.set("n", step_out, dap.step_out)
+      vim.keymap.set("n", step_back, dap.step_back)
+      vim.keymap.set("n", restart, dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
+}
